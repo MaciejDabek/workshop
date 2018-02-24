cdef59a9cd573427a183d2abedd06c0066f3bc6d
unknown <maciex92@gmail.com>
2018-02-24 11:55:49 +0100

    utworzenie projektu Math

---
+++ Math.Tests/MathTest.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+using Shouldly;
+
+namespace Math.Tests
+{
+    public class MathTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            //arrange:
+
+            // act:
+
+            // assert:
+        }
+    }
+}
