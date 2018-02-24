cd65a57e88c736212547af96dcad5c00de504d1a
unknown <maciex92@gmail.com>
2018-02-24 15:03:18 +0100

    utworzenie klasy LinearFunction i klasy testowej LinearFunctionTest; implementacja pierwszej metody

--- LinearFunction.Tests/LinearFunctionTest.cs
+++ LinearFunction.Tests/LinearFunctionTest.cs
@@ -1,14 +1,47 @@
 using System;
 using Xunit;
+using Shouldly;
 
 namespace LinearFunction.Tests
 {
     public class LinearFunctionTest
     {
         [Fact]
-        public void Test1()
+        public void set_default_value_a_to_1__when__is_not_setted_and_b_to_0_when_is_not_setted()
         {
+            //arrange:
+            var fun = new LinearFunction(new parametr_a(), new parametr_b());
+            //act:
 
+            //assert:
+            fun.parametr_a.Value.ShouldBe(1);
+            fun.parametr_b.Value.ShouldBe(0);
         }
+
+        [Fact]
+        public void formula_length_greater_than_0__when____a_and_b_are_setted()
+        {
+            //arrange:
+            var fun = new LinearFunction(new parametr_a(2), new parametr_b(-7));
+
+            //act:
+            var printView = fun.PrintViewOfFormula();
+
+            //assert:
+            printView.Length.ShouldBeGreaterThan(0);
+        }
+
+        [Fact]
+        public void throw_exception__when__a_and_b_set_to_0()
+        {
+            //arrange:
+            var fun = new LinearFunction(new parametr_a(0), new parametr_b(0));
+
+            //act:
+            Action action = () => fun.PrintViewOfFormula();
+
+            //assert:
+            Should.Throw<FunctionParametrs_a_and_b_SetToZeroException>(action);
+        }        
     }
 }
--- LinearFunction/LinearFunction.cs
+++ LinearFunction/LinearFunction.cs
@@ -4,5 +4,82 @@ namespace LinearFunction
 {
     public class LinearFunction
     {
+        public parametr_a parametr_a { get; private set; }
+        public parametr_b parametr_b { get; private set; }
+        public LinearFunction(parametr_a a, parametr_b b)
+        {
+            this.parametr_a = a;
+            this.parametr_b = b;
+        }
+
+        public string PrintViewOfFormula()
+        {
+            if (parametr_a.Value == 0 && parametr_b.Value == 0)
+            {
+                throw new FunctionParametrs_a_and_b_SetToZeroException();
+            }
+            var ax = parametr_a.Value + "x";
+            var b = parametr_b.Value;
+
+            return String.Format("f(x) = {0} {1}", ax, b);
+        }
     }
+
+    public class FunctionParametrs_a_and_b_SetToZeroException : Exception
+    {
+
+    }
+
+    public class parametr_a
+    {
+        private const int _defaultValue = 1;
+        private int? _value;
+        public int Value
+        {
+            get
+            {
+                if (_value == null)
+                {
+                    return _defaultValue;
+                }
+                return _value.Value;
+            }
+        }
+
+        public parametr_a(int value)
+        {
+            _value = value;
+        }
+
+        public parametr_a()
+        {
+        }
+    }
+
+    public class parametr_b
+    {
+        private const int _defaultValue = 0;
+        private int? _value;
+        public int Value
+        {
+            get
+            {
+                if (_value == null)
+                {
+                    return _defaultValue;
+                }
+                return _value.Value;
+            }
+        }
+
+        public parametr_b(int value)
+        {
+            _value = value;
+        }
+
+        public parametr_b()
+        {
+        }
+    }
+
 }
