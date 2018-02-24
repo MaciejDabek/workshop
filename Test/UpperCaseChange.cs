f5804f64a5029d174ad92c05f987f209878bd14c
Krzysztof Owsiany <gocom@o2.pl>
2018-02-24 10:58:40 +0100

    Feature 1

---
+++ Test/UpperCaseChange.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace UpperCase
+{
+    public class UpperCaseChange
+    {
+        public String Change(String text){
+            return text.ToUpper();
+        }
+    }
+}
